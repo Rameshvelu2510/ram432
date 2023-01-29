23ba38bbc583eef328f3ee519e4de1e575042b97
ramesh <Dell@DESKTOP-BD2QFT4>
2023-01-29 23:11:11 +0530

    second commit

--- ram.py
+++ ram.py
@@ -6,12 +6,10 @@ Get started by creating a new file or uploading an existing file. We recommend e
 …or create a new repository on the command line
 echo "# ram432" >> README.md
 git init
-git add README.md
 git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/Rameshvelu2510/ram432.git
 git push -u origin main
 …or push an existing repository from the command line
 git remote add origin https://github.com/Rameshvelu2510/ram432.git
-git branch -M main
 git push -u origin main
